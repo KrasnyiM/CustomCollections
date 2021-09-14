43b1b9cd8df0123474a657c63da875ca339f078a
KrasnyiM <miroslavchervonij01@gmail.com>
2021-09-14 20:21:32 +0300

    #4 Implement array-based ring buffer

---
+++ DelphiTask_1/ArrRingBuffer.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelphiTask_1
+{
+    class ArrRingBuffer : IFunc
+    {
+
+        private int[] arrBuffer;
+        private int head;
+        private int tail;
+        /// <inheritdoc />
+        public int Count { get; private set; }
+        /// <summary>
+        /// Constructor for initializing fields.
+        /// </summary>
+        /// <param name="length"></param>
+        public ArrRingBuffer(int length)
+        {
+            head = 0;
+            tail = 0;
+            arrBuffer = new int[length];
+        }
+        /// <inheritdoc />
+        public int Peek()
+        {
+            if (Count == 0)
+            {
+                return default;
+            }
+            return arrBuffer[tail];
+        }
+        /// <inheritdoc />
+        public void Push(int value)
+        {
+            if (Count == arrBuffer.Length)
+            {
+                return;
+            }
+
+            arrBuffer[head] = value;
+            Count++;
+            head = (head + 1) % arrBuffer.Length;             
+        }
+        /// <inheritdoc />
+        public int Pop()
+        {
+            if(Count == 0)
+            {
+                return default;
+            }
+
+            int result = arrBuffer[tail];
+            Count--;
+            tail = (tail+1) % arrBuffer.Length;            
+            return result;
+        }
+    }
+}
