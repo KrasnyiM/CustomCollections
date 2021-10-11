b6efeeb8880e8ff3d1d24386d638da6390f2041a
KrasnyiM <78660666+KrasnyiM@users.noreply.github.com>
2021-10-11 14:25:05 +0300

    Revert "Merge branch 'master' into feature/8-add-linked-list"

--- DelphiTask_1/ArrQueue.cs
+++ DelphiTask_1/ArrQueue.cs
@@ -6,9 +6,6 @@ using System.Threading.Tasks;
 
 namespace DelphiTask_1
 {
-    /// <summary>
-    /// MyLinkeList, Class which implement array based queue.
-    /// </summary>
     public class ArrQueue : IFunc
     {
         private int[] arrayQueue;
